1cc4a71ad8f6e5569ac0623d84dbd45cc966463c
Monica <monica@fortalezatec.com.br>
2021-11-29 20:32:42 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -2,20 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ marginTop: 100 }}>
+      <Text style={{ color: "#ff0000", fontSize: 30 }}>Sujeito Programador</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;  //exporta para que outros componentes possam usar
